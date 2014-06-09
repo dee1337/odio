d3e74f87c4ccdb0118a4a45883137801b7c36718
Philip Nuzhnyy <philip.nuzhnyy@gmail.com>
2014-06-09 19:18:34 +0200

    setup build flow

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,11 +11,38 @@ module.exports = function(grunt) {
                     base: './'
                 }
             }
+        },
+
+        useminPrepare: {
+            html: 'dist/index.html',
+            options: {
+                root: './',
+                dest: 'dist'
+            }
+        },
+
+        usemin: {
+            html: 'dist/index.html'
+        },
+
+        copy: {
+            main: {
+                files: [
+                    {expand: true, src: ['index.html'], dest: 'dist/'},
+                    {expand: true, src: ['images/**/*'], dest: 'dist/'}
+                ]
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-usemin');
 
     // Default task(s).
     grunt.registerTask('default', ['connect:server']);
+    grunt.registerTask('build', ['copy', 'useminPrepare', 'concat', 'uglify', 'cssmin', 'usemin']);
 };
